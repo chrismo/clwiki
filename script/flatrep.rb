397b85ac983df83e1e57534bb1ae95ef003c7fef
chrismo <chrismo@clabs.org>
2015-03-08 16:36:31 -0500

    flatrep helper script

---
+++ script/flatrep.rb
@@ -0,0 +1,19 @@
+# read file - append to dest file with tags as first line
+# dump output on dupes for review, but still just append
+
+require 'fileutils'
+
+root = File.expand_path(File.dirname(__FILE__))
+dest = root + "_flat"
+FileUtils.makedirs(dest)
+
+Dir[root + '/**/*.txt'].each do |pathfilename|
+  dest_fn = File.join(dest, File.basename(pathfilename))
+  tags = File.dirname(pathfilename.sub(root, '')).split('/')
+  puts "Dupe: #{File.basename(pathfilename)}" if File.exists?(dest_fn)
+  File.open(dest_fn, 'a') do |out|
+    out.puts "\ntags: #{tags.join(' ')}"
+    out.puts
+    out.print File.read(pathfilename)
+  end
+end
