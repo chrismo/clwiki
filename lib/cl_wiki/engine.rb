0426cd429c5ab7fe2fc197584cfa262645aaffba
chrismo <chrismo@clabs.org>
2015-09-17 22:01:32 -0500

    Add asset precomp init to engine.rb

--- lib/cl_wiki/engine.rb
+++ lib/cl_wiki/engine.rb
@@ -1,5 +1,9 @@
 module ClWiki
   class Engine < ::Rails::Engine
     isolate_namespace ClWiki
+
+    initializer 'cl_wiki.assets.precompile' do |app|
+      app.config.assets.precompile += %w(application.css application.js)
+    end
   end
 end
