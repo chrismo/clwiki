236063ec198d33fd0c58f8c2f12cd6f293943d42
chrismo <chrismo@clabs.org>
2019-12-24 22:01:08 -0600

    cleanup a warning

--- config/initializers/clwiki.rb
+++ config/initializers/clwiki.rb
@@ -6,4 +6,3 @@ conf_hash = YAML::load(::File.open(filename))[Rails.env]
 conf_hash['wiki_path'] = ::File.expand_path(conf_hash['wiki_path'], Rails.root)
 $wiki_conf = ClWiki::Configuration.new(conf_hash)
 $wiki_path = $wiki_conf.wiki_path
-$wiki_conf
