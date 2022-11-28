9b2de2be912de19ad1c4cd4d6d77aafd2a76fa1f
chrismo <chrismo@clabs.org>
2022-11-28 09:28:57 -0600

    3.1.2 - 2 fixes
    - override monospace default textarea style in chrome
    - fix shortcuts.js to ignore /login, and on /recent to allow `f`

--- app/assets/javascripts/cl_wiki/shortcuts.js
+++ app/assets/javascripts/cl_wiki/shortcuts.js
@@ -1,17 +1,23 @@
+function navigateToFind() {
+  let url = new URL(window.location.href).href;
+  window.location.href = url.substring(0, url.lastIndexOf('/')) + '/find';
+}
+
 $(document).keydown(function (e) {
   if (window.location.href.endsWith("/edit")) {
-
   } else if (window.location.href.endsWith("/find")) {
-
+  } else if (window.location.href.endsWith("/login")) {
+    // this is important otherwise typing `e` while logging in refreshes the
+    // page - doh!
+  } else if (window.location.href.endsWith("/recent")) {
+    if (e.key === "f") {
+      navigateToFind();
+    }
   } else {
     if (e.key === "e") {
       window.location.href = window.location.href + "/edit";
     } else if (e.key === "f") {
-      let url;
-      let findPath;
-      url = new URL(window.location.href).href;
-      findPath = url.substring(0, url.lastIndexOf('/')) + '/find';
-      window.location.href = findPath;
+      navigateToFind();
     }
   }
 });
